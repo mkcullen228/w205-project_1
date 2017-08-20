2629a29949314ed317987adc2210396fac83bcc6
Bala Ganeshan <ganesb@berkeley.edu>
2017-08-20 18:38:25 +0000

    Added SQL script to create tables

--- to_postgres/create_tables.py
+++ to_postgres/create_tables.py
@@ -7,7 +7,51 @@
 #   We presume Hive has been set up to use Postgres database with
 #   the name metastore.
 #
+import subprocess
+import sys
+import os
 
-p = subprocess.Popen(['psql',
-                      '-t -U metastore -e "select \"SD_ID\",\"TBL_NAME\" from \"TBLS\" where \"TBL_TYPE\" = 'MANAGED_TABLE';"'])
-print p.communicate()
+p_id = os.getpid()
+os.chdir('/tmp')
+f_name = 'eBags_tables_' + str(p_id) + '.out'
+
+p = subprocess.check_call(['psql', '-t', '-o', f_name, '-U', 'hiveuser', 'metastore', '-c', 
+                      'select "SD_ID","TBL_NAME" from "TBLS" where "TBL_TYPE" = \'MANAGED_TABLE\';'])
+#
+#  read the file back in
+#
+tables = open(f_name, 'r').readlines()
+
+table_dict = {}
+for lines in tables:
+    if lines.strip():
+        table_dict.update({lines.split('|')[1].strip() : lines.split('|')[0].strip()})
+
+p = os.remove(f_name)
+
+#
+
+f_name = "eBags_columns_" + str(p_id)
+f_tablenames = []
+for keys in table_dict.keys():
+    f_name_table = f_name + table_dict[keys] + ".out"
+    f_tablenames.append(f_name_table)
+    select_stmt = 'select c2."CD_ID", c2."COLUMN_NAME", c2."TYPE_NAME", tbls."TBL_NAME", tbls."LOCATION" \
+                   FROM "COLUMNS_V2" AS c2 \
+                   JOIN (SELECT * FROM "TBLS" JOIN "SDS" ON "TBLS"."SD_ID" = "SDS"."SD_ID") AS tbls \
+                   ON c2."CD_ID" = tbls."CD_ID" WHERE tbls."CD_ID" = ' + table_dict[keys] + ';'
+    p = subprocess.check_call(['psql', '-t', '-o', f_name_table, '-U', 'hiveuser', 'metastore', '-c', 
+                   select_stmt], stdout=subprocess.PIPE)
+    #
+    #   Now build the tables in the default database, ebags_weblog, using the information stored in the file
+    #
+    col = open(f_name_table, 'r').readlines()
+    
+    col_dict = {}
+    for lines in col:
+        if lines.strip():
+            col_dict.update({lines.split('|')[1].strip() : lines.split('|')[2].strip()})
+
+    print keys
+    print col_dict
+
---
+++ to_postgres/create_tables.sql
@@ -0,0 +1,105 @@
+--
+-- First create the database that will store all the processed weblog data
+--
+-- It is presumed the user is starting the script logged on as user "postgres"
+-- We will create the database and transfer the ownership to user "hiveuser"
+-- 
+CREATE DATABASE ebags_weblog
+       WITH OWNER = hiveuser;
+--
+-- Create tables in preparation for importing data from HDFS into postgres
+--
+-- The tables are created in the database we just created
+--
+\c ebags_weblog
+--
+--  The weblog table
+--
+CREATE TABLE  weblog
+ (
+  utc_datetime TIMESTAMP,
+  mt_datetime TIMESTAMP,
+  utc_time_ms INT,
+  s_ip VARCHAR,
+  cs_method VARCHAR,
+  s_sitename VARCHAR,
+  cs_uri_stem VARCHAR,
+  s_computername VARCHAR,
+  cs_uri_query VARCHAR,
+  cs_user_agent VARCHAR,
+  s_port VARCHAR,
+  cs_referer VARCHAR,
+  cs_host VARCHAR,
+  sc_status VARCHAR,
+  sc_win32_status VARCHAR,
+  sc_bytes BIGINT,
+  cs_bytes BIGINT,
+  time_taken_ms BIGINT,
+  visitor_id_hash VARCHAR
+ );
+--
+-- Filtering R = (Robot, crawler, spider); S = (Spam or bad bot) User-Agents from weblogs (see User-Agents.org)
+--
+CREATE TABLE  weblog_user_agents
+ (     
+  cs_user_agent VARCHAR
+ );
+
+CREATE TABLE  user_agent_bots_spiders
+ (
+  id VARCHAR,
+  signature VARCHAR,
+  description VARCHAR,
+  type VARCHAR
+ );
+
+CREATE TABLE  user_agent_exclusion
+ (
+   user_agent VARCHAR
+ );
+
+CREATE TABLE  weblog_filtered
+ (
+  utc_datetime TIMESTAMP,
+  mt_datetime TIMESTAMP,
+  utc_time_ms INT,
+  s_ip VARCHAR,
+  cs_method VARCHAR,
+  s_sitename VARCHAR,
+  cs_uri_stem VARCHAR,
+  s_computername VARCHAR,
+  cs_uri_query VARCHAR,
+  cs_user_agent VARCHAR,
+  s_port VARCHAR,
+  cs_refere VARCHAR,
+  cs_host VARCHAR,
+  sc_status VARCHAR,
+  sc_win32_status VARCHAR,
+  sc_bytes BIGINT,
+  cs_bytes BIGINT,
+  time_taken_ms BIGINT,
+  visitor_id_hash VARCHAR,
+  user_agent VARCHAR
+ );
+
+--
+--Create Table for Model Page Views
+--
+CREATE TABLE  visitor_model_view
+ (
+  mt_date DATE,
+  visitor_id_hash VARCHAR,
+  model_id INT,
+  model_views BIGINT
+ );
+--
+--Create Table to Track Add to Carts (atc)
+--
+CREATE TABLE  visitor_model_atc
+ (
+  mt_date DATE,
+  visitor_id_hash VARCHAR,
+  model_id INT,
+  product_id INT,
+  model_atc BIGINT
+ );
