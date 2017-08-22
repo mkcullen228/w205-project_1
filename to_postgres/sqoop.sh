8ccec80b188d2c826cb13cdb22deecde984c3102
Bala Ganeshan <ganesb@berkeley.edu>
2017-08-22 20:38:24 +0000

    Updating Sqoop shell script

--- to_postgres/sqoop.sh
+++ to_postgres/sqoop.sh
@@ -4,49 +4,55 @@
 #
 #  It assumes
 #
-#   1. The database, ebags_weblog, has been created a priori
+#   1. The database, prod_cat_stats, has been created a priori
 #   2. ANT is installed on the UCB provided AMI
 #   3. Packages, asciidoc and xmlto, has been installed on the image
 #   4. Sqoop version 1.5.0 has been downloaded from Apache github repository and compiled.
 #   5. Hive has been set up to use Postgres and the the weblog pipelining job has been executed.
 #
-#   Import the weblog data from HDFS
+#   Set the location of the Hive "warehouse" in HDFS.
+#
+#   **** THIS HAS TO BE CHANGED IF HIVE STORES ITS DATA IN A DIFFERENT LOCATION (OR MACHINE) ***
+#
+HiveHDFS = hdfs://localhost:8020/user/hive/warehouse
+#
+#   Next import weblog data from HDFS
 #
 sqoop export --input-null-string '\\N' --input-null-non-string '\\N' --connect jdbc:postgresql://localhost/ebags_weblog \
-       --username hiveuser --input-fields-terminated-by "\t" --export-dir=hdfs://localhost:8020/user/hive/warehouse/weblog/ --table=weblog
+       --username hiveuser --input-fields-terminated-by "\t" --export-dir=${HiveHDFS}/weblog/ --table=weblog
 #
 #   Import weblog user agent information from HDFS
 #
 sqoop export --input-null-string '\\N' --input-null-non-string '\\N'  --connect jdbc:postgresql://localhost/ebags_weblog \
-    --username hiveuser --input-fields-terminated-by "\t" --export-dir=hdfs://localhost:8020/user/hive/warehouse/weblog_user_agents/ \
+    --username hiveuser --input-fields-terminated-by "\t" --export-dir=${HiveHDFS}/weblog_user_agents/ \
     --table=weblog_user_agents
 #
 #   Import weblog bots and spiders information from HDFS
 #
 sqoop export --input-null-string '\\N' --input-null-non-string '\\N' --connect jdbc:postgresql://localhost/ebags_weblog \
-    --username hiveuser --input-fields-terminated-by "\t" --export-dir=hdfs://localhost:8020/user/hive/warehouse/user_agent_bots_spiders/ \
+    --username hiveuser --input-fields-terminated-by "\t" --export-dir=${HiveHDFS}/user_agent_bots_spiders/ \
     --table=user_agent_bots_spiders
 #
 #   Import user agent exclusion information from HDFS
 #
 sqoop export --input-null-string '\\N' --input-null-non-string '\\N' --connect jdbc:postgresql://localhost/ebags_weblog \
-    --username hiveuser --input-fields-terminated-by "\t" --export-dir=hdfs://localhost:8020/user/hive/warehouse/user_agent_exclusion/ \
+    --username hiveuser --input-fields-terminated-by "\t" --export-dir=${HiveHDFS}/user_agent_exclusion/ \
     --table=user_agent_exclusion
 #
 #   Import filtered weblog data from HDFS
 #
 sqoop export --input-null-string '\\N' --input-null-non-string '\\N' --connect jdbc:postgresql://localhost/ebags_weblog \
-    --username hiveuser --input-fields-terminated-by "\001" --export-dir=hdfs://localhost:8020/user/hive/warehouse/weblog_filtered/ \
+    --username hiveuser --input-fields-terminated-by "\001" --export-dir=${HiveHDFS}/weblog_filtered/ \
     --table=weblog_filtered
 #
 #    Import visitor information from HDFS
 #
 sqoop export --input-null-string '\\N' --input-null-non-string '\\N' --connect jdbc:postgresql://localhost/ebags_weblog \
-    --username hiveuser --input-fields-terminated-by "\001" --export-dir=hdfs://localhost:8020/user/hive/warehouse/visitor_model_view/ \
+    --username hiveuser --input-fields-terminated-by "\001" --export-dir=${HiveHDFS}/visitor_model_view/ \
     --table=visitor_model_view
 #
 #    Import visitor information with models from HDFS
 #
 sqoop export --input-null-string '\\N' --input-null-non-string '\\N' --connect jdbc:postgresql://localhost/ebags_weblog \
-    --username hiveuser --input-fields-terminated-by "\001" --export-dir=hdfs://localhost:8020/user/hive/warehouse/visitor_model_atc/ \
+    --username hiveuser --input-fields-terminated-by "\001" --export-dir=${HiveHDFS}/visitor_model_atc/ \
     --table=visitor_model_atc
