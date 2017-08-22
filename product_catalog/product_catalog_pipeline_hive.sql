a59c6a6fc14929ad2673f1dfc7ab0e95a54d0731
Maura Cullen <mcullen@jawbone.com>
2017-08-22 16:13:59 -0700

    getting up to date with master

--- product_catalog/product_catalog_pipeline_hive.sql
+++ product_catalog/product_catalog_pipeline_hive.sql
@@ -1,21 +1,23 @@
+-- #For Amazon S3 library access (as user w205)
+--once as w205: $export HADOOP_CLASSPATH=/usr/lib/hadoop/client
+--run hive as w205:
+SET fs.s3.impl=org.apache.hadoop.fs.s3native.NativeS3FileSystem;
+SET fs.s3.awsSecretAccessKey=INSERT SECRET ACCESS KEY FROM AWS ACCOUNT HERE;
+SET fs.s3.awsAccessKeyId=INSERT ACCESS KEY FROM AWS ACCOUNT HERE;- #For XML classes (as root):
+
 -- #For XML classes:
---once- cd /usr/lib/hadoop/lib
---once- wget -o hivexmlserde-1.0.5.3.jar http://search.maven.org/remotecontent?filepath=com/ibm/spss/hive/serde2/xml/hivexmlserde/1.0.5.3/hivexmlserde-1.0.5.3.jar
---once- chmod 777 hivexmlserde-1.0.5.3.jar
+--once as root: $cd /usr/lib/hadoop-0.20-mapreduce/lib
+--once as root: $wget -O http://search.maven.org/remotecontent?filepath=com/ibm/spss/hive/serde2/xml/hivexmlserde/1.0.5.3/hivexmlserde-1.0.5.3.jar hivexmlserde-1.0.5.3.jar 
+--once as root: $chmod 777 hivexmlserde-1.0.5.3.jar
 add jar /usr/lib/hadoop-0.20-mapreduce/lib/hivexmlserde-1.0.5.3.jar;
---
---
--- #For Product Catalog File
---once- hdfs dfs -mkdir /user/w205/project_1/product_catalog
---once- hdfs dfs -put "/home/w205/project_1/eBags_ChannelAdvisor_CSEAndKeywordsProducts_20170705_115248.xml" /user/w205/project_1/product_catalog/eBags_ChannelAdvisor_CSEAndKeywordsProducts_20170705_115248.xml
---
 
 
 --
 --  Product Catalog
 --
-DROP TABLE IF EXISTS model_level_info_raw;
-CREATE EXTERNAL TABLE model_level_info_raw 
+
+DROP TABLE IF EXISTS product_catalog_xml_raw;
+CREATE EXTERNAL TABLE product_catalog_xml_raw 
 (model_id STRING, status STRING, brand STRING, description STRING, images STRING, features STRING, attributes STRING, facets STRING, product_rating STRING, master_category STRING, sub_category STRING,
   model_url STRING, review_count STRING, color_count STRING)
  ROW FORMAT SERDE 'com.ibm.spss.hive.serde2.xml.XmlSerDe'
@@ -37,15 +39,17 @@ CREATE EXTERNAL TABLE model_level_info_raw
  )
  STORED AS INPUTFORMAT 'com.ibm.spss.hive.serde2.xml.XmlInputFormat'
  OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
-  LOCATION '/user/w205/project_1/product_catalog/'
+  LOCATION 's3://midsw205project1/product_catalog'
   TBLPROPERTIES ("xmlinput.start"="<ProductCatalogItem>","xmlinput.end"= "</ProductCatalogItem>");
+  
+select * from product_catalog_xml_raw where model_id='1_1';
   
    
 DROP TABLE IF EXISTS model_images;
 CREATE TABLE model_images AS
 SELECT regexp_extract(model_id,'(\\d+)_\\d+') as model_id, regexp_extract(model_id,'\\d+_(\\d+)') as model_price_group,
   a.model_image_url as model_image_url
-FROM model_level_info_raw 
+FROM product_catalog_xml_raw 
 LATERAL VIEW explode(xpath(images, 'string/ImageUrl/text()')) a as model_image_url
 WHERE images is not NULL;
 
@@ -60,7 +64,7 @@ DROP TABLE model_images;
 DROP TABLE IF EXISTS model_features;
 CREATE TABLE model_features AS
 SELECT regexp_extract(model_id,'(\\d+)_\\d+') as model_id, regexp_extract(model_id,'\\d+_(\\d+)') as model_price_group, a.model_feature as model_feature
-FROM model_level_info_raw
+FROM product_catalog_xml_raw
 LATERAL VIEW explode(xpath(features, 'string/DescriptionText/text()')) a as model_feature
 WHERE features is not NULL;
 
@@ -75,7 +79,7 @@ DROP TABLE model_features;
 DROP TABLE IF EXISTS model_attributes;
 CREATE TABLE model_attributes AS
 SELECT regexp_extract(model_id,'(\\d+)_\\d+') as model_id, regexp_extract(model_id,'\\d+_(\\d+)') as model_price_group, a.description as description, b.measure_value as measure_value, c.uom as uom, d.group_code as group_code, e.measure_type as measure_type
-FROM model_level_info_raw
+FROM product_catalog_xml_raw
   LATERAL VIEW posexplode(xpath(attributes,'string/Description/text()')) a as pos_a, description
   LATERAL VIEW posexplode(xpath(attributes,'string/MeasureValue/text()')) b as pos_b, measure_value
   LATERAL VIEW posexplode(xpath(attributes,'string/UnitOfMeasureCode/text()')) c as pos_c, uom
@@ -96,7 +100,7 @@ DROP TABLE model_features;
 DROP TABLE IF EXISTS model_facets;
 CREATE TABLE model_facets AS
 SELECT regexp_extract(model_id,'(\\d+)_\\d+') as model_id, regexp_extract(model_id,'\\d+_(\\d+)') as model_price_group, a.name as facet_name, b.value as facet_value
-FROM model_level_info_raw
+FROM product_catalog_xml_raw
                   LATERAL VIEW posexplode(xpath(facets,'string/AttributeName/text()')) a as pos_a, name
                   LATERAL VIEW posexplode(xpath(facets,'string/AttributeValue/text()')) b as pos_b, value
 WHERE facets is not NULL
@@ -109,35 +113,78 @@ SELECT cast(model_id as int) as model_id, cast(min(model_price_group) as int) as
 FROM model_facets
 GROUP BY model_id, model_price_group, facet_name, facet_value;
 DROP TABLE model_facets;
--->
+
 DROP TABLE IF EXISTS model_grouped;
 CREATE TABLE model_grouped AS
 SELECT cast(regexp_extract(model_id,'(\\d+)_\\d+') as int) as model_id, 
-       cast(min(regexp_extract(model_id,'\\d+_(\\d+)')) as int) as default_price_group, 
+       min(cast(regexp_extract(model_id,'\\d+_(\\d+)') as int)) as default_price_group, 
        cast(count(distinct regexp_extract(model_id,'\\d+_(\\d+)')) as int) as price_group_count,
-  min(status) as status, min(brand) as brand, min(description) as description, min(product_rating) as product_rating,
-  min(master_category) as master_category, min(sub_category) as sub_category, min(model_url) as model_url,
+  min(status) as status, max(brand) as brand, max(description) as description, max(product_rating) as product_rating,
+  max(master_category) as master_category, max(sub_category) as sub_category, max(model_url) as model_url,
   cast(min(review_count) as int) as review_count, cast(min(color_count) as int) as color_count
-FROM model_level_info_raw
-GROUP BY
-  model_id;
+FROM product_catalog_xml_raw
+GROUP BY cast(regexp_extract(model_id,'(\\d+)_\\d+') as int);
 
-DROP TABLE IF EXISTS model;
-CREATE TABLE model AS
+DROP TABLE IF EXISTS model_image_count;
+CREATE TABLE model_image_count AS
 SELECT m.model_id as model_id, m.default_price_group as default_price_group, m.price_group_count as price_group_count,
   m.status, m.brand, m.description, m.product_rating, m.master_category, m.sub_category, m.model_url, 
   cast(min(review_count) as int) as review_count, cast(min(color_count) as int) as color_count,
-  cast(count(distinct mi.model_image_url) as int) as image_count, cast(count(distinct mft.model_feature) as int) as feature_count, 
-  cast(count(distinct ma.description) as int) as attribute_count, cast(count(distinct mfa.facet_name) as int) as facet_count
+  cast(count(distinct mi.model_image_url) as int) as image_count
 FROM model_grouped m
 LEFT OUTER JOIN model_images_grouped mi ON (m.model_id = mi.model_id AND m.default_price_group = mi.default_price_group)
+GROUP BY m.model_id, m.default_price_group, m.price_group_count, m.status, m.brand, m.description, m.product_rating, m.master_category, m.sub_category, 
+  m.model_url, m.review_count, m.color_count;
+
+DROP TABLE IF EXISTS model_feature_count;
+CREATE TABLE model_feature_count AS
+SELECT m.model_id as model_id, m.default_price_group as default_price_group, m.price_group_count as price_group_count,
+  m.status, m.brand, m.description, m.product_rating, m.master_category, m.sub_category, m.model_url, 
+  cast(min(review_count) as int) as review_count, cast(min(color_count) as int) as color_count,
+  cast(count(distinct mft.model_feature) as int) as feature_count
+FROM model_grouped m
 LEFT OUTER JOIN model_features_grouped mft ON (m.model_id = mft.model_id AND m.default_price_group = mft.default_price_group)
+GROUP BY m.model_id, m.default_price_group, m.price_group_count, m.status, m.brand, m.description, m.product_rating, m.master_category, m.sub_category, 
+  m.model_url, m.review_count, m.color_count;
+  
+DROP TABLE IF EXISTS model_attribute_count;
+CREATE TABLE model_attribute_count AS
+SELECT m.model_id as model_id, m.default_price_group as default_price_group, m.price_group_count as price_group_count,
+  m.status, m.brand, m.description, m.product_rating, m.master_category, m.sub_category, m.model_url, 
+  cast(min(review_count) as int) as review_count, cast(min(color_count) as int) as color_count,
+  cast(count(distinct ma.description) as int) as attribute_count
+FROM model_grouped m
 LEFT OUTER JOIN model_attributes_grouped ma ON (m.model_id = ma.model_id AND m.default_price_group = ma.default_price_group)
+GROUP BY m.model_id, m.default_price_group, m.price_group_count, m.status, m.brand, m.description, m.product_rating, m.master_category, m.sub_category, 
+  m.model_url, m.review_count, m.color_count;
+
+DROP TABLE IF EXISTS model_facet_count;
+CREATE TABLE model_facet_count AS
+SELECT m.model_id as model_id, m.default_price_group as default_price_group, m.price_group_count as price_group_count,
+  m.status, m.brand, m.description, m.product_rating, m.master_category, m.sub_category, m.model_url, 
+  cast(min(review_count) as int) as review_count, cast(min(color_count) as int) as color_count,
+  cast(count(distinct mfa.facet_name) as int) as facet_count
+FROM model_grouped m
 LEFT OUTER JOIN model_facets_grouped mfa ON (m.model_id = mfa.model_id AND m.default_price_group = mfa.default_price_group)
 GROUP BY m.model_id, m.default_price_group, m.price_group_count, m.status, m.brand, m.description, m.product_rating, m.master_category, m.sub_category, 
   m.model_url, m.review_count, m.color_count;
-DROP TABLE model_grouped;
+  
+DROP TABLE IF EXISTS model;
+CREATE TABLE model AS
+SELECT mi.model_id as model_id, mi.default_price_group as default_price_group, mi.price_group_count as price_group_count,
+  mi.status, mi.brand, mi.description, mi.product_rating, mi.master_category, mi.sub_category, mi.model_url, mi.image_count as image_count,
+  fe.feature_count as feature_count, ma.attribute_count as attribute_count, fa.facet_count as facet_count
+FROM model_image_count mi, model_feature_count fe, model_attribute_count ma, model_facet_count fa
+WHERE mi.model_id = fe.model_id and mi.model_id = ma.model_id and mi.model_id = fa.model_id
+  AND mi.default_price_group = fe.default_price_group and mi.default_price_group = ma.default_price_group and mi.default_price_group = fa.default_price_group;
 
+DROP TABLE model_grouped;
+  
+DROP TABLE IF EXISTS model_image_count;
+DROP TABLE IF EXISTS model_feature_count;
+DROP TABLE IF EXISTS model_attribute_count;
+DROP TABLE IF EXISTS model_facet_count;
+  
 --Validating the Tables:
 
 SELECT * FROM model
@@ -154,5 +201,4 @@ WHERE model_id='1' AND model_price_group = '1';
 
 SELECT * FROM model_facets
 WHERE model_id='1' AND model_price_group = '1';
-
 
